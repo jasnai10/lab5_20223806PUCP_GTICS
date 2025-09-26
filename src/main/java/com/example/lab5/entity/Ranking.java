3669434b61cf5d4d218d7347f4954e4c262e5809
Jair <a20223806@pucp.edu.pe>
2025-09-25 21:06:46 -0500

    feat: Subiendo archivos

--- src/main/java/com/example/lab5/entity/Ranking.java
+++ src/main/java/com/example/lab5/entity/Ranking.java
@@ -2,7 +2,6 @@ package com.example.lab5.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
-import org.hibernate.annotations.SQLJoinTableRestriction;
 
 @Entity
 @Table(name = "ranking")
