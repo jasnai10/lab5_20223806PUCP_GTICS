27fd4429a68bb7f49e438c6167733543b5db284c
Jair <a20223806@pucp.edu.pe>
2025-09-25 21:10:56 -0500

    feat: Subiendo archivos finales

--- src/main/java/com/example/lab5/entity/Ranking.java
+++ src/main/java/com/example/lab5/entity/Ranking.java
@@ -1,10 +1,32 @@
 package com.example.lab5.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "ranking")
 public class Ranking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="usuario_id", nullable=false)
+    private Integer id;
 
+    @Column(name = "total_regalos")
+    private Integer totalRegalos;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getTotalRegalos() {
+        return totalRegalos;
+    }
+
+    public void setTotalRegalos(Integer totalRegalos) {
+        this.totalRegalos = totalRegalos;
+    }
 }
